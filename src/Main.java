0dc4d69a2f94bdaa6b8957c446d772fd75219574
Niklas Istenes <niklas@retroid.com>
2020-10-07 14:21:33 +0200

    Added message to ask the user for input and removed all the class files

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("Hello World");
+
+        System.out.println("Guess a number between 1 and 5:");
     }
 
 }
