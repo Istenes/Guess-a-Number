beba0c8a39ee241bd5039b8b14276bb4e50c894c
Niklas Istenes <niklas@retroid.com>
2020-10-07 14:35:25 +0200

    - Added GetRandomNumberUtil stub - Updated gitignore with more folders - Added a Scanner to accept input from user

---
+++ src/GetRandomNumberUtil.java
@@ -0,0 +1,2 @@
+public class GetRandomNumberUtil {
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
         System.out.println("Hello World");
 
         System.out.println("Guess a number between 1 and 5:");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
     }
 
 }
