6def513ce9460d346722afc867aa39565117651d
Niklas Istenes <niklas@retroid.com>
2020-10-07 14:48:13 +0200

    Changed world to Earth

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Hello Earth");
 
         System.out.println("Guess a number between 1 and 5:");
         Scanner scanner = new Scanner(System.in);
